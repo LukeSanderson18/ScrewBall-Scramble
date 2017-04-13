122d801271acf921c44292af561ea4b63fdd8dda
LukeSanderson18 <cactuar18@hotmail.com>
2017-04-14 00:10:51 +0100

    crane functionality - magnet

--- Assets/ButtonScript.cs
+++ Assets/ButtonScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ButtonScript : MonoBehaviour {
 
-    public enum ButtonType { Bridge };
+    public enum ButtonType { Bridge , Crane };
 
     public ButtonType myButtonType;
 
@@ -12,6 +12,16 @@ public class ButtonScript : MonoBehaviour {
 
     public Rigidbody[] bridges;
     public float bridgeForce = 200f;
+
+    //
+
+    public Transform craneMagnet;
+    public Rigidbody crane;
+    public Rigidbody metalBall;
+    public float magnetForce = 100f;
+    public float magnetDistance = 2f;
+
+    //
 	
 	void Update () {
 
@@ -19,6 +29,9 @@ public class ButtonScript : MonoBehaviour {
         {
             case ButtonType.Bridge:
                 Bridge();
+                break;
+            case ButtonType.Crane:
+                Crane();
                 break;
             default:
                 print("forgot to assign button type to " + gameObject.name);
@@ -43,5 +56,21 @@ public class ButtonScript : MonoBehaviour {
             }
         }
         
+    }
+
+    void Crane()
+    {
+        if (Vector3.Distance(craneMagnet.position,metalBall.transform.position) < magnetDistance)       //if close enough, do magnetism.
+        {
+            print("do it");
+            metalBall.AddExplosionForce(magnetForce * -1, craneMagnet.position, magnetDistance);
+            //(Vector3.Lerp(metalBall.transform.position, craneMagnet.position, Time.deltaTime));
+            //metalBall.AddForce((craneMagnet.position - metalBall.transform.position) * magnetForce);
+            //magnetForce = Vector3.Distance(craneMagnet.position, metalBall.transform.position) * 250;
+            //magforce =  1 divided by the distance between the two squared.
+        }
+
+        crane.AddTorque(Vector3.up * Input.GetAxis("Horizontal") * 2);
+
     }
 }
