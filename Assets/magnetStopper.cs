9de91ab19b3562c80d193778b7a7315529018887
LukeSanderson18 <cactuar18@hotmail.com>
2017-04-14 01:37:32 +0100

    aaaaaaaaand the crane stopped working.

---
+++ Assets/magnetStopper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class magnetStopper : MonoBehaviour {
+
+    public GameObject magnet;
+    public GameObject magnetStopperObj;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == magnetStopperObj)
+        {
+            print("hit");
+            magnet.transform.position = Vector3.one * 100;  
+        }
+    }
+}
