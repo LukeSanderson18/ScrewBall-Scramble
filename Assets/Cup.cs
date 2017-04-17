d44706d561061fa6fb9bd8046319c4933ba1a51b
LukeSanderson18 <cactuar18@hotmail.com>
2017-04-17 15:53:47 +0100

    spring

---
+++ Assets/Cup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cup : MonoBehaviour {
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Flip();
+        }
+    }
+    void Flip()
+    {
+        rb.AddForceAtPosition(Vector3.up * 3000, transform.GetChild(0).position);
+    }
+}
