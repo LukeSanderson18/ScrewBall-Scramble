5d535dd52722f6dbab697bac79b279d38ab8af68
LukeSanderson18 <cactuar18@hotmail.com>
2017-05-21 13:21:20 +0100

    Likkle fix

---
+++ Assets/CamScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamScript : MonoBehaviour {
+
+    public Transform target;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetPostition = new Vector3(target.position.x*0.25f,
+                                        target.transform.position.y *0.25f,
+                                        target.position.z *0.25f);
+        transform.LookAt(targetPostition);
+        //transform.LookAt(target);
+	}
+}
--- Assets/Cup.cs
+++ Assets/Cup.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Cup : MonoBehaviour {
 
     Rigidbody rb;
+    public BoxCollider bc;
 
     private void Start()
     {
@@ -15,10 +16,16 @@ public class Cup : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Return))
         {
             Flip();
+            Invoke("Kill", 0.2f);
         }
     }
     void Flip()
     {
-        rb.AddForceAtPosition(Vector3.up * 3000, transform.GetChild(0).position);
+        rb.AddForceAtPosition(Vector3.up * 9000, transform.GetChild(0).position);
+    }
+
+    void Kill()
+    {
+        bc.enabled = false;
     }
 }
